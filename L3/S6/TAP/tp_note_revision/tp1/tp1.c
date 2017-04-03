d6359e3bd1b45feb8c5d5be4ae85b768bf068f23
faudoy <faudoy@etu.u-bordeaux.fr>
2017-04-03 23:20:53 +0200

    avancement du tp noté

--- L3/S6/TAP/tp_note_revision/tp1/tp1.c
+++ L3/S6/TAP/tp_note_revision/tp1/tp1.c
@@ -28,6 +28,7 @@ void number_addition(number *R, number *X, number *Y){
     ind_r ++;
   }
 
+  //Ajout des chiffres restant dans le plus long nombre
   if(n_min->n < n_max->n){
     for (int i = n_min->n; i< n_max->n; ++i){
       res = n_max->digit[i] + retenue; // Addition chiffre par chiffre
@@ -36,9 +37,10 @@ void number_addition(number *R, number *X, number *Y){
       ind_r ++;
       }
   }
+  //Ajout de la retenue à la fin
   if(retenue != 0)
     R->digit[ind_r] = retenue ;
-  
+    
 
   return;
 }
@@ -46,18 +48,79 @@ void number_addition(number *R, number *X, number *Y){
 
 // calcule R = X-Y en O(n), en supposant que X>=Y
 void number_substraction(number *R, number *X, number *Y){
+
+  short retenue = 0, res;
+  for(int i=0; i < Y->n ; ++i){
+    res = X->digit[i] - (Y->digit[i] + retenue);
+    retenue = res < 0 ? 1 : 0;
+    R->digit[i] = res < 0 ? res + BASE : res ;
+  }
+
+  if(X->n > Y->n){
+    for(int i = Y->n; i < X->n; ++i){
+      res = X->digit[i] - retenue;
+      retenue = res < 0 ? 1 : 0;
+      R->digit[i] = res < 0 ? res + BASE : res;
+    }
+  }
+
+    /*if(retenue != 0)
+      R->digit[X->n] = retenue;*/
+      
   return;
 }
 
 
 // calcule R = X*Y selon la méthode standard en O(n^2)
 void number_mul_standard(number *R, number *X, number *Y){
+
+  number *tab_res[X->n];
+
+  //O(n)
+  for(int i = 0; i< X->n; ++i){
+    tab_res[i] = number_new(10); //Je suppose 10 assez grand pour que le resultat d'une multiplication puisse rentrer
+    *tab_res[i]->digit =  X->digit[i] * (*Y->digit);
+  }
+  
+  //O(n)
+  for(int i = 0; i < X->n; ++i){
+    R->digit[i] += (*tab_res[i]->digit);
+    number_free(tab_res[i]);
+  }
+    
   return;
 }
 
 
 // calcule R = X*Y selon la méthode récursive en O(n^2)
 void number_mul_recursive(number *R, number *X, number *Y){
+
+  if(X->n == 1 || Y->n == 1){
+   *R->digit = (*X->digit) * (*Y->digit);
+   return;
+  }
+  
+  int m = max(X->n, Y->n) / 2; 
+
+  number* x_plus = number_new(X->n - m);
+  number* x_moins = number_new(m);
+  number* y_plus = number_new(Y->n - m);
+  number* y_moins = number_new(m);
+
+  number* p1 = number_new(10); //Je suppose 10 assez grand pour contenir un resultat
+  number* p2 = number_new(10);
+  number *p3 = number_new(10);
+  number *p4 = number_new(10);
+
+  number_mul_recursive(p1,x_plus,y_plus);
+  number_mul_recursive(p2,x_plus,y_moins);
+  number_mul_recursive(p3,x_moins,y_plus);
+  number_mul_recursive(p4,x_moins,y_moins);
+
+  short a = *p1->digit + *p2->digit;
+  
+  *R->digit = ((*p1->digit*(BASE^(2*m))) + (a*(BASE^m))+ *p4->digit);
+  
   return;
 }
 
