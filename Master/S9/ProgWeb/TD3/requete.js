62c77ff969e4f323f747f7bbc5d11009aa4dfda4
FrancoisAudoy <francois.audoy@gmail.com>
2018-09-27 19:35:09 +0200

    J'ai reussi à faire une requete en javascript

--- Master/S9/ProgWeb/TD3/requete.js
+++ Master/S9/ProgWeb/TD3/requete.js
@@ -1,21 +1,24 @@
 let toilette;
-let rep;
 
 function onSuccess(resp) {
-    if(rep.state === "fullfiled")
-	console.log("I did it");
-    console.log(resp);
-    return resp;
+    let data = JSON.stringify(resp);
+    let parsed = JSON.parse(data);
+    console.log(parsed);
+    parsed.d.forEach(function(element) {
+	console.log(element.nom);
+	$("#data-list").append("<li>"+ element.nom+ "</li>");
+    });
+    
 }
 
 function onError() {
     console.log("error");
 }
 
-let url_test = "https://randomuser.me/api/?results=10,format=json";
+let url_test = "https://randomuser.me/api/?results=10&format=json";
 
 let myheaderBDX = new Headers();
-myheaderBDX.append('Accept', 'application/json, text/plain, */*');
+myheaderBDX.append('Accept', 'application/json, text/plain, text/json, */*');
 myheaderBDX.append("Content-Type", "application/json");
 myheaderBDX.append("Access-Control-Allow-Origin", "origin");
 
@@ -25,7 +28,7 @@ let initBDX = {method : 'GET' ,
 	    cache : 'default'
 	   };
 
-function doFetch(url, init){
+async function doFetch(url, init){
     return fetch(url, init)
 	.then(onSuccess)
 	.catch(function(error){
@@ -42,14 +45,15 @@ function get_test (){
 
 }
 
+
 function get_toilettes_location (event){
-    let url_toilettes = 'http://opendata.bordeaux.fr/content/toilettes-publiques/?format=json';
-    rep = doFetch(url_test, initBDX );
+    let url_toilettes = 'http://odata.bordeaux.fr/v1/databordeaux/sigsanitaire/?format=json&callback=?';
+    $.getJSON(url_toilettes, onSuccess);
 }
 
 function get_kid_area_location (event){
-    let url_play_area = 'http://opendata.bordeaux.fr/aires-de-jeux/?format=json';
-    rep = doFetch(url_test, initBDX);
+    let url_play_area = 'http://odata.bordeaux.fr/v1/databordeaux/airejeux/?format=json&callback=?'
+    $.getJSON(url_play_area, onSuccess);
 }
 
 
@@ -58,5 +62,3 @@ function get_kid_area_location (event){
 ----------*/
 $( "#btn-toilets").on("click",get_toilettes_location);
 $( "#btn-kidareas").click(get_kid_area_location);
-
-get_test();
