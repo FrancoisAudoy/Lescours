ec116334ccdf5adfd3c493f3bafe6e7d5bd367e9
FrancoisAudoy <francois.audoy@gmail.com>
2018-11-02 08:23:04 +0100

    Avancement TP5 Web

--- Master/S9/ProgWeb/TP5/HelloWorld/src/fr/ub/m2gl/UserResource.java
+++ Master/S9/ProgWeb/TP5/HelloWorld/src/fr/ub/m2gl/UserResource.java
@@ -1,16 +1,16 @@
 package fr.ub.m2gl;
 
+import java.util.ArrayList;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
-import org.bson.BSON;
 import org.bson.Document;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
@@ -34,8 +34,8 @@ public class UserResource {
 
 	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
-	public User AddUser (@PathParam("firstname") String firstname, 
-			@PathParam("lastname") String lastname){
+	public User AddUser (@QueryParam("firstname") String firstname, 
+			@QueryParam("lastname") String lastname){
 
 		try {
 
@@ -45,61 +45,65 @@ public class UserResource {
 			ObjectMapper mapper = new ObjectMapper();
 			User user = new User(firstname, lastname);
 			String jsonString = mapper.writeValueAsString(user);
-			
+
 			Document doc = Document.parse(jsonString);
 			collection.insertOne(doc);
-			
+
 			return user;
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 			return null;
 
-		} finally {
-			mc.close();
 		}
 	}
 
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public User ChangeUser(@PathParam("firstname") String firstname, 
-			@PathParam("lastname") String lastname, 
-			@PathParam("newfirstname") String newFirstName, @PathParam("newlastname") String newLastName) {
+	public User ChangeUser(@QueryParam("firstname") String firstname, 
+			@QueryParam("lastname") String lastname, 
+			@QueryParam("newfirstname") String newFirstName, @QueryParam("newlastname") String newLastName) {
 		MongoDatabase bdd = mc.getDatabase("application");
 		ObjectMapper mapper = new ObjectMapper();
-		MongoCollection<Document> collection = bdd.getCollection("user"); 
+		MongoCollection<Document> collection = bdd.getCollection("user");
+
 		Document query = new Document();
 		query.append("firstname", firstname).append("lastname", lastname);
 		Document update = new Document();
 		update.append("firstname", newFirstName).append("lastname", newLastName);
-		
+
 		collection.updateOne(query, update);
 		return new User(newFirstName, newLastName);
 	}
-	
+
 	@DELETE
-	public void removeUser(@PathParam("firstname") String firstname, 
-			@PathParam("lastname") String lastname){
+	public void removeUser(@QueryParam("firstname") String firstname, 
+			@QueryParam("lastname") String lastname){
+		
+		MongoDatabase bdd = mc.getDatabase("application");
+		MongoCollection<Document> collection = bdd.getCollection("user");
+		Document userToRemove = new Document();
+		userToRemove.append("firstname", firstname).append("lastname", lastname);
+		collection.findOneAndDelete(userToRemove);
 	}
-	
-	@Path("all")
+
+	@Path("/all")
 	@GET
-	@Produces("application/json")
-	public String GetAllUsers (){
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<User> GetAllUsers (){
 
-		try {
-			MongoDatabase bdd = mc.getDatabase("application");
-			MongoCollection<Document> collection = bdd.getCollection("user");
-
-			FindIterable<Document> doc = collection.find();
-
-			ObjectMapper mapper = new ObjectMapper();
-
-			return mapper.writeValueAsString(doc);
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
+		MongoDatabase bdd = mc.getDatabase("application");
+		MongoCollection<Document> collection = bdd.getCollection("user");
+		
+		ArrayList<User> users = new ArrayList<>();
+		ArrayList<Document> doc = (ArrayList<Document>)collection.find().into(new ArrayList<Document>());
+		
+		for(Document user : doc) {
+			String fname = user.getString("firstname");
+			String lname = user.getString("lastname");
+			users.add(new User(fname,lname));
 		}
+		
+		return users;
 	}
 
 }
---
+++ Master/S9/ProgWeb/TP5/contact/contact.js
@@ -0,0 +1,12 @@
+let API_URL = "localhost:8080/ContactAPI/app/"
+
+class Contact
+
+class App {
+
+    let allContact;
+    contructor(){}
+
+    start(){
+	fillListContact();
+	
