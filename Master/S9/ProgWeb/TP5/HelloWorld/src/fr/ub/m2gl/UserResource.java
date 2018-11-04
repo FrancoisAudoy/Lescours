2fb12c0a855685377bcde449c610bce6423e3f45
FrancoisAudoy <francois.audoy@gmail.com>
2018-11-04 17:45:59 +0100

    Fin du tp5-ProgWeb

--- Master/S9/ProgWeb/TP5/HelloWorld/src/fr/ub/m2gl/UserResource.java
+++ Master/S9/ProgWeb/TP5/HelloWorld/src/fr/ub/m2gl/UserResource.java
@@ -1,5 +1,6 @@
 package fr.ub.m2gl;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.ws.rs.*;
@@ -8,13 +9,14 @@ import javax.ws.rs.core.MediaType;
 import org.bson.Document;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-@Path("/user")
+@Path("/users")
 public class UserResource {
 
 	ObjectMapper mapper;
@@ -34,16 +36,17 @@ public class UserResource {
 
 	@PUT
 	@Produces(MediaType.APPLICATION_JSON)
-	public User AddUser (@QueryParam("firstname") String firstname, 
-			@QueryParam("lastname") String lastname){
+	public User AddUser (String data) throws IOException{
 
 		try {
-
+			
 			MongoDatabase bdd = mc.getDatabase("application");
 			MongoCollection<Document> collection = bdd.getCollection("user");
-
 			ObjectMapper mapper = new ObjectMapper();
-			User user = new User(firstname, lastname);
+			JsonNode rootNode = mapper.readTree(data);
+			JsonNode fnameNode = rootNode.get("firstname");
+			JsonNode lnameNode = rootNode.get("lastname");
+			User user = new User(fnameNode.asText(), lnameNode.asText());
 			String jsonString = mapper.writeValueAsString(user);
 
 			Document doc = Document.parse(jsonString);
@@ -59,43 +62,83 @@ public class UserResource {
 
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public User ChangeUser(@QueryParam("firstname") String firstname, 
-			@QueryParam("lastname") String lastname, 
-			@QueryParam("newfirstname") String newFirstName, @QueryParam("newlastname") String newLastName) {
+	public User ChangeUser(String data) throws JsonProcessingException, IOException {
 		MongoDatabase bdd = mc.getDatabase("application");
 		ObjectMapper mapper = new ObjectMapper();
 		MongoCollection<Document> collection = bdd.getCollection("user");
 
+		JsonNode rootNode = mapper.readTree(data);
+		JsonNode oldFirstName = rootNode.get("oldFirstName");
+		JsonNode oldLastName = rootNode.get("oldLastName");
+		JsonNode newFirstName = rootNode.get("newFirstName");
+		JsonNode newLastName = rootNode.get("newLastName");
+		
 		Document query = new Document();
-		query.append("firstname", firstname).append("lastname", lastname);
+		query.append("firstname", oldFirstName.asText())
+		.append("lastname", oldLastName.asText());
 		Document update = new Document();
-		update.append("firstname", newFirstName).append("lastname", newLastName);
-
-		collection.updateOne(query, update);
-		return new User(newFirstName, newLastName);
+		update.append("firstname", newFirstName.asText())
+		.append("lastname", newLastName.asText());
+		Document operation = new Document("$set", update);
+		collection.updateOne(query, operation);
+		return new User(newFirstName.asText(), newLastName.asText());
 	}
 
 	@DELETE
-	public void removeUser(@QueryParam("firstname") String firstname, 
-			@QueryParam("lastname") String lastname){
+	public void removeUser(String data) throws JsonProcessingException, IOException{
 		
 		MongoDatabase bdd = mc.getDatabase("application");
 		MongoCollection<Document> collection = bdd.getCollection("user");
+		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode rootNode = mapper.readTree(data);
+		JsonNode firstnameNode = rootNode.get("firstname");
+		JsonNode lastnameNode = rootNode.get("lastname");
 		Document userToRemove = new Document();
-		userToRemove.append("firstname", firstname).append("lastname", lastname);
+		userToRemove.append("firstname", firstnameNode.asText())
+		.append("lastname", lastnameNode.asText());
 		collection.findOneAndDelete(userToRemove);
 	}
 
 	@Path("/all")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public ArrayList<User> GetAllUsers (){
+	public ArrayList<User> GetAllUsers () throws JsonProcessingException{
 
+		MongoDatabase bdd = mc.getDatabase("application");
+		MongoCollection<Document> collection = bdd.getCollection("user");
+		ObjectMapper mapper = new ObjectMapper();
+		ArrayList<User> users = new ArrayList<>();
+		ArrayList<Document> doc = (ArrayList<Document>)collection.find().into(new ArrayList<Document>());
+		
+		for(Document user : doc) {
+			String fname = user.getString("firstname");
+			String lname = user.getString("lastname");
+			users.add(new User(fname,lname));
+		}
+		
+		return users;
+	}
+		
+	@Path("/user")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<User> GetUserByName(@QueryParam("firstname") String firstname, 
+			@QueryParam("lastname") String lastname) throws IOException {
 		MongoDatabase bdd = mc.getDatabase("application");
 		MongoCollection<Document> collection = bdd.getCollection("user");
 		
 		ArrayList<User> users = new ArrayList<>();
-		ArrayList<Document> doc = (ArrayList<Document>)collection.find().into(new ArrayList<Document>());
+
+		Document query = new Document();
+		if( firstname != null && firstname != "null")
+			query.append("firstname", firstname);
+		
+		if( lastname!= null && lastname != "null") 
+			query.append("lastname", lastname);
+		
+		
+		ArrayList<Document> doc = (ArrayList<Document>)collection.find(query).into(new ArrayList<Document>());
 		
 		for(Document user : doc) {
 			String fname = user.getString("firstname");
--- Master/S9/ProgWeb/TP5/contact/contact.js
+++ Master/S9/ProgWeb/TP5/contact/contact.js
@@ -1,12 +1,141 @@
-let API_URL = "localhost:8080/ContactAPI/app/"
+let API_URL = "http://127.0.0.1:8080/ContactAPI/app/users"
+let format = "?format=json&callback=?";
 
-class Contact
+class request {
 
-class App {
+    constructor(url_api, format){
+	this.url = url_api;
+	this.format = format;
+	this.defaultCallback = this.defaultCallback.bind(this);
+    }
 
-    let allContact;
-    contructor(){}
+    defaultCallback(req,res){
+	console.log(req);
+    }
+    
+    send(url, type_req, data = null, callback = this.defaultCallback, useDefaultFormat = true){
+	let finalUrl = this.url + url;
+	if(useDefaultFormat)
+	    finalUrl += format;
+	$.ajax({
+	    header : {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json'
+	    },
+	    url : finalUrl,
+	    type : type_req,
+	    datatype : "json",
+	    data : data
+	}).done(callback);
+    }
+}
 
-    start(){
-	fillListContact();
+class application {
+
+    constructor() {
+	this.reloadContactList = this.reloadContactList.bind(this);
+	this.createContact = this.createContact.bind(this);
+	this.changeContact = this.changeContact.bind(this);
+	this.deleteContact = this.deleteContact.bind(this);
+	this.findContact = this.findContact.bind(this);
+	this.req = new request(API_URL, format);
+    }
+
+    refreshListContact(res){
+
+	$("#list-contact").html('');
+
+	let jsonString = JSON.stringify(res);
+	let data = JSON.parse(jsonString);
+
+	data.forEach(function(element) {
+	    $("#list-contact").append('<li class="list-group-item">' +
+				      element.firstname + " " + element.lastname);
+	    $("#list-contact").append('</li>');
+	});
+    }
 	
+    reloadContactList(){
+	this.req.send("/all","GET", null, this.refreshListContact);
+	    
+    }
+
+    refreshWithoutReload(res){
+	let jsonString = JSON.stringify(res);
+	let data = JSON.parse(jsonString);
+	
+	$("#list-contact").append('<li class="list-group-item">' +
+				  data.firstname + " " + data.lastname);
+	$("#list-contact").append('</li>');
+    }
+
+    
+    createContact() {
+	let fname = document.getElementById("add-fn").value;
+	let lname = document.getElementById("add-ln").value;
+	let contact = {firstname : fname, lastname : lname};
+	console.log(JSON.stringify(contact));
+	this.req.send("","PUT", JSON.stringify(contact), this.refreshWithoutReload);
+    }
+
+    changeContact() {
+	let oldfname = document.getElementById("change-old-fn").value;
+	let oldlname = document.getElementById("change-old-ln").value;
+	let newfname = document.getElementById("change-new-fn").value;
+	let newlname = document.getElementById("change-new-ln").value;
+
+	let correctContact = {
+	    oldFirstName : oldfname,
+	    oldLastName : oldlname,
+	    newFirstName : newfname,
+	    newLastName : newlname
+	}
+	console.log(JSON.stringify(correctContact));
+	this.req.send("","POST",JSON.stringify(correctContact));
+	this.reloadContactList();
+    }
+
+    deleteContact(){
+	let fname = document.getElementById("suppr-fn").value;
+	let lname = document.getElementById("suppr-ln").value;
+
+	let contact = {firstname : fname, lastname : lname};
+	this.req.send("","DELETE", JSON.stringify(contact), null);
+	this.reloadContactList();
+    }
+
+    fillFoundList(res){
+	$("#found-contact").html('');
+
+	let jsonString = JSON.stringify(res);
+	let data = JSON.parse(jsonString);
+
+	data.forEach(function(element) {
+	    $("#found-contact").append('<li class="list-group-item">' +
+				      element.firstname + " " + element.lastname);
+	    $("#found-contact").append('</li>');
+	});
+    }
+    
+    findContact(){
+	let fname = document.getElementById("find-fn").value;
+	let lname = document.getElementById("find-ln").value;
+
+	let url = "/user?firstname="+fname + "&lastname=" + lname +
+	    "format=json&callback=?";
+
+	this.req.send(url, "GET",null,this.fillFoundList,false);
+	
+    }
+    
+    run() {
+	$("#add-contact").click(this.createContact);
+	$("#change-contact").click(this.changeContact);
+	$("#suppr-contact").click(this.deleteContact);
+	$("#find-contact").click(this.findContact);
+	this.reloadContactList();
+    }
+}
+
+let app = new application();
+app.run();
